bbcdcb3c58cf5a4e39bb109e0358a5967288d8a9
lucaSalmiHB <luca.salmi@hackberry.se>
2023-05-04 10:45:13 +0200

    Initial Commit

---
+++ examens_arbete_ios/examens_arbete_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  examens_arbete_iosApp.swift
+//  examens_arbete_ios
+//
+//  Created by Luca Salmi on 2023-05-04.
+//
+
+import SwiftUI
+
+@main
+struct examens_arbete_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
